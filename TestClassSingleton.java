59c1fa838d9f3c3db4d2343ccd09481448005374
Roussel Rakoto <hei.roussel11@gmail.com>
2023-06-01 11:44:06 +0300

    feat: add TestClassSingleton

---
+++ TestClassSingleton.java
@@ -0,0 +1,10 @@
+public class TestClassSingleton {
+    public static void main(String[] args) {
+        ClassSingleton classSingleton1 = ClassSingleton.getInstance();
+        ClassSingleton classSingleton2 = ClassSingleton.getInstance();
+
+        System.out.println("Test ClassSingleton");
+        System.out.println("1: "+classSingleton1.g);
+        System.out.println("2: "+classSingleton2);
+    }
+}
---
+++ TestClassUniqueInstance.java
@@ -0,0 +1,2 @@
+public class TestClassUniqueInstance {
+}
