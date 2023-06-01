8d4a7b29b9960f4751c69bb18da0e80dece98be7
Roussel Rakoto <hei.roussel11@gmail.com>
2023-06-01 10:56:44 +0300

    feat: add ClassSingleton

---
+++ ClassSingleton.java
@@ -0,0 +1,14 @@
+public final class ClassSingleton {
+    private static ClassSingleton INSTANCE;
+    private String description;
+
+    private ClassSingleton() {
+    }
+    public static ClassSingleton getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ClassSingleton();
+        }
+
+        return INSTANCE;
+    }
+}
